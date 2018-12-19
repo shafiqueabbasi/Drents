43489a67262511b3a6e87835ce97cbe0f719a4b0
shafiqabbasi <shafiqanwarabbasi@gmail.com>
2018-12-19 14:52:09 +0500

    remove waste code

--- client/src/Components/firstPage.js
+++ client/src/Components/firstPage.js
@@ -32,29 +32,3 @@ class FirstPage extends Component {
 }
 
 export default FirstPage;
-
-{/*<div className="row">
-      		 	<div className="col-md-6" style={{backgroundColor: "black"}}>
-      		 		<div className="row">
-      		 			<div className="col-md-4">
-      		 				<img src="./images/Drent-logo-white.png" style={{width: '100%'}}/>
-      		 			</div>
-      		 			<div className="col-md-4"></div>
-      		 			<div className="col-md-4"></div>
-      		 		</div>
-      		 	</div>
-      		 	<div className="col-md-6" style={{height: '129px'}}>
-      		 		<div className="row">
-      		 			<div className="col-md-1" style={{backgroundColor: "yellow", height: '129px'}}></div>
-      		 			<div className="col-md-10" style={{backgroundColor: "black", height: '129px'}}>
-      		 				<div className="row">
-		      		 			<div className="col-md-3"></div>
-		      		 			<div className="col-md-3"></div>
-		      		 			<div className="col-md-3"></div>
-		      		 			<div className="col-md-3"></div>
-		      		 		</div>
-      		 			</div>
-      		 			<div className="col-md-1" style={{backgroundColor: "blue", height: '129px'}}></div>
-      		 		</div>
-      		 	</div>
-      		</div>*/}
