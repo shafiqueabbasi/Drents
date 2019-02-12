3a98540341e337a1eee98d6807c6d1f93e83da11
devdaniyal <daniyalrazanoorani@gmail.com>
2019-02-12 14:55:38 +0500

    profile ui changes

--- client/src/Components/_components/myInput.js
+++ client/src/Components/_components/myInput.js
@@ -12,7 +12,7 @@ export const RadioInput = props => {
                 type="radio" 
                 id={props.for} 
                 name={props.name}
-                /*style={{position: 'absolute', opacity: '0', cursor: 'pointer', height: '0', width: '0'}}*/
+                style={{position: 'absolute', opacity: '0', cursor: 'pointer', height: '0', width: '0'}}
                 value={props.value}
                 onChange={props.onChange}
             />
@@ -25,12 +25,12 @@ export const RadioInput = props => {
 export const TextInput = props => {
     return(
         <div>
-            <div className="col-md-2 col-sm-2">
+            <div className={props.col}>
                 <span className="input">
                     <h3>{props.label}</h3>
                 </span>
             </div>
-            <div className="col-sm-4 col-sm-4">
+            <div className={props.col2}>
                 <div className="inputBox ">
                     <div className="inputText"></div>                    
                     <input 
@@ -48,17 +48,17 @@ export const TextInput = props => {
 export const SelectInput = props => {
     return(
         <div>
-            <label htmlFor={props.name} className="col-md-2 col-sm-2">
+            <label htmlFor={props.name} className={props.col}>
                 {props.label}
             </label>
-            <div className="col-sm-4 col-sm-4">
+            <div className={props.col2}>
                 <select required 
-                    className="col-sm-12 col-sm-12"
+                    className="col-md-12 col-sm-12"
                     placeholder={props.placeholder} 
                     name={props.name} 
                     id={props.id} 
                     value={props.value} 
-                    onChange={props.Change}>
+                    onChange={props.Change}  style={{width:'100%'}}>
                     {props.options && props.options.map((ob)=><option key={ob.id} value={ob}>{ob}</option>)}
                 </select>
             </div>
