bb81c4a7ee4e7a089a9a9504bc0edc8e9348da22
shafiqabbasi <shafiqanwarabbasi@gmail.com>
2019-02-14 16:39:39 +0500

    ui set

--- client/src/Components/home/heading4.js
+++ client/src/Components/home/heading4.js
@@ -12,26 +12,26 @@ class Heading4 extends Component {
       		</div>}
       		<div className="container-fluid">
 	    		<div className="container-fluid">
-	    			<div className="col-md-12">
+	    			<div className="col-md-12 col-sm-12">
 	    				<div className="row" style={{textAlign:'center'}}>
-	    					<div className="col-md-4">
-	    						<img src="./images/productdetail/dress1.jpg" className="dress1" style={{width:'100%'}} />
+	    					<div className="col-md-4 col-sm-4">
+	    						<img src="./images/productdetail/dress1.jpg" className="dress1" style={{width:'90%'}} />
 	    							<div>
 	    								<h2 className="h_dress">Sheta</h2>
 	    								<h3 className="h_dress">Lase Trim Shirts</h3>
 	    								<h3 className="h_dress">$ 400</h3>
 	    							</div>
 	    					</div>
-	    					<div className="col-md-4">
-	    						<img src="./images/productdetail/dress2.jpg" className="dress2" style={{width:'100%'}}/>
+	    					<div className="col-md-4 col-sm-4">
+	    						<img src="./images/productdetail/dress2.jpg" className="dress2" style={{width:'90%'}}/>
 	    							<div>
 	    								<h2 className="h_dress">Sheta</h2>
 	    								<h3 className="h_dress">Lase Trim Shirts</h3>
 	    								<h3 className="h_dress">$ 400</h3>
 	    							</div>
 	    					</div>
-	    					<div className="col-md-4">
-	    						<img src="./images/productdetail/dress3.jpg" className="dress3" style={{width:'100%'}}/>
+	    					<div className="col-md-4 col-sm-4">
+	    						<img src="./images/productdetail/dress3.jpg" className="dress3" style={{width:'90%'}}/>
 	    						<div>
 		    						<div>
 		    							<h2 className="h_dress">Sheta</h2>
@@ -41,7 +41,7 @@ class Heading4 extends Component {
 		    					</div>
 	    					</div>
 	    					
-	    					{hrLine && <div className="col-md-12">
+	    					{hrLine && <div className="col-md-12 col-sm-4">
 	    						<hr/>
 	    					</div>}
 	    				</div>
