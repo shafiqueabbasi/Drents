3036d2b28d3a794bc0958276d815175f545c06b5
DevFarzan <farzanhanif123@gmail.com>
2019-02-16 16:24:31 +0500

    changing in all

---
+++ controllers/getProfile.js
@@ -0,0 +1,17 @@
+const profile = require('../models/profile');
+
+
+//get dreses for user with all
+exports.getProfile = function(req,res,next){
+profile.find({"userId":req.body.userId},function(err,specificProfile){
+  if(err){
+    return res.status(422).send({error:'No Profile Found!'})
+  }
+  if(specificProfile){
+    res.send({
+	    code:200,
+	    specificProfile
+    });
+  }
+})
+}
