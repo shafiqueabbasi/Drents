f3eca5da5b6f4753edd1af6dcd0e1bdc9845f268
DevFarzan <farzanhanif123@gmail.com>
2019-02-19 18:24:05 +0500

    changing in all

--- router.js
+++ router.js
@@ -12,9 +12,7 @@ const requireAuth = passport.authenticate('jwt', { session:false });
 const requireSignin = passport.authenticate('local', {session:false});
 
 module.exports = function(app){
-  app.get('/',requireAuth, function(req, res){
-    res.send({hi:'there'});
-  });
+  
   //post routes
   app.post('/signin',requireSignin, Authentication.signin);
   app.post('/signup',Authentication.signup);
