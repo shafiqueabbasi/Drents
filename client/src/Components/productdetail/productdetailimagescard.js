91c17121dc2d1dc234502a2316ae5cfbceb6a414
devdaniyal <daniyalrazanoorani@gmail.com>
2019-03-21 16:57:15 +0500

    changing in product page

--- client/src/Components/productdetail/productdetailimagescard.js
+++ client/src/Components/productdetail/productdetailimagescard.js
@@ -1,4 +1,5 @@
  import React, { Component } from 'react';
+ import { isMobileOnly, isTablet } from "react-device-detect";
  import './productdetail.css'
 
  class Productdetailimagescard extends Component {
@@ -13,12 +14,13 @@
 
 	render() { 
  		const { data } = this.props;
-
+ 			console.log(isTablet, 'ye tablet hai')
+ 			console.log(isMobileOnly, 'ye mobile hai')
 		return( 
 			<div>
 				<div className="hidden-xs hidden-sm">
 					<div className="row">
-						<div  className="col-md-3 hidden-xs hidden-sm">
+						<div className="col-md-3 hidden-xs hidden-sm">
 							{data.map((elem) => {
 								return (
 									<span>
@@ -42,15 +44,15 @@
 				</div> 	
             	<br/><br/><br/>
             	<div className="visible-sm visible-xs">
-            	<div className="container">
-            		<div className="row">
-            			<div className="col-xs-12 col-sm-12">
-            				<img alt="" src={this.state.showImg} style={{width:"100%", height:"875px"}}/>
-            			</div>
-            		</div><br />
-            		<div className="row">	
-            			{data.map((elem) => {
-							return (
+	            	<div className="container">
+	            		<div className="row">
+	            			<div className="col-xs-12 col-sm-12">
+	            				<img alt="" src={this.state.showImg} style={isTablet ? {width:"92%", height:"840px" , marginTop:"-13%"} : {width:"70%", height:"400px"}}/>
+	            			</div>
+	            		</div><br />
+	            		<div className="row" style={{margin:'0px'}}>	
+	            			{data.map((elem) => {
+								return (
 								<div className="col-sm-4 col-xs-4">
 									<img 
 										alt="" 
@@ -61,9 +63,9 @@
 										}
 									/>
 								</div>
-							)
-						})}						
-					</div>
+								)
+							})}						
+						</div>
 					</div>	
 				</div>
 			</div>
