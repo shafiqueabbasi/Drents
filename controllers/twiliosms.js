6a01032fea022ead276bd7b37ed2e7b8413bc37a
DevFarzan <farzanhanif123@gmail.com>
2019-03-26 17:15:58 +0500

    changing in all

--- controllers/twiliosms.js
+++ controllers/twiliosms.js
@@ -3,8 +3,8 @@ const twilio = require('twilio');
 
 
 exports.twilioSms = function(req, res, next){
-  var accountSid = 'AC2d7daf51473bbeb1adb316e08dcb6074'; // Your Account SID from www.twilio.com/console
-var authToken = 'e24ea9d0bf757e890decce1224cce314';   // Your Auth Token from www.twilio.com/console
+  var accountSid = 'ACc61df5933ded243c57059f573b13fa4b'; // Your Account SID from www.twilio.com/console
+var authToken = '8c134331cf082a979fb05cdd842db101';   // Your Auth Token from www.twilio.com/console
 
 var twilio = require('twilio');
 var client = new twilio(accountSid, authToken);
@@ -12,7 +12,7 @@ var client = new twilio(accountSid, authToken);
 client.messages.create({
     body: 'Hello from Drent testing message',
     to: '+923412346257',  // Text this number
-    from: '+12016361763' // From a valid Twilio number
+    from: '+12028314091' // From a valid Twilio number
 })
 .then((message) =>{
   res.send({
