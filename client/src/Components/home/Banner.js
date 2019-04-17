143eae52648e397cb4769051ba3049f3caf6ef51
shafiqabbasi <shafiqanwarabbasi@gmail.com>
2019-04-17 12:16:02 +0500

    changing in all

--- client/src/Components/home/Banner.js
+++ client/src/Components/home/Banner.js
@@ -7,12 +7,12 @@ class SecondPage extends Component {
     <div>
       <div className="row App B_H hidden-xs">
       	<div className="row Banner">
-      		<img alt="" src="../images/Banner-2.jpg" style={{width: '100%'}}/>
+      		<img alt="" src="https://res.cloudinary.com/dxk0bmtei/image/upload/v1555483736/banner-2_krkshp.jpg" style={{width: '100%'}}/>
       	</div>
       </div>
       <div className="App B_H visible-xs">
       	<div className="Banner">
-      		<img alt="" src="../images/Banner-2.jpg" style={{width: '100%'}}/>
+      		<img alt="" src="https://res.cloudinary.com/dxk0bmtei/image/upload/v1555483736/banner-2_krkshp.jpg" style={{width: '100%'}}/>
       	</div>
       </div>
     </div>
