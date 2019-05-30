ea606d3f7e47f64d907ad685307d47a523c426ed
shafiqabbasi <shafiqanwarabbasi@gmail.com>
2019-05-30 14:45:32 +0500

    changing

--- client/src/App.js
+++ client/src/App.js
@@ -78,7 +78,7 @@ class App extends Component {
 			<BrowserRouter>
 	          <div>
                 {/*<HeaderNew />*/}
-	             {/*<Header arr={this.state.arr}/>*/}
+	             <Header arr={this.state.arr}/>
 	              <Route path="/" exact component={Home} />
                 <Route path="/profile/:value" render={props => <UserProfile {...props} updateFooter={this.updateFooter}/>} />
                 <Route path="/product" component={MainPage} />
