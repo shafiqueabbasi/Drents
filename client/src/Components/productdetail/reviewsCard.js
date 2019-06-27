7e2e870e74c05e0f7b2ce924523dfeb1f5851710
devdaniyal <daniyalrazanoorani@gmail.com>
2019-06-27 15:49:31 +0500

    UI NEW

--- client/src/Components/productdetail/reviewsCard.js
+++ client/src/Components/productdetail/reviewsCard.js
@@ -8,108 +8,49 @@ class ReviewsCard extends Component {
     
     return(
           <div>
-            <div className="row dell hidden-xs hidden-sm">
-                <div className="container" style={{padding:"22px"}}>
-                  <div className="col-md-12">
-                    <div className="col-md-3">
-                      <Link to={`/profile/${data.userId}`}>
-                          <h4 className="tissue" style={{fontFamily: 'Sacramento',fontSize: '30px'}}>{data.name}</h4><br/>
-                      </Link>
+              <div className="row">
+                  <div className="col-xs-12 col-sm-10 col-md-9 col-lg-9">
                       <div className="row">
-                          <div className="col-md-7 row" style={{padding: '0px'}}>
-                            <p className="tissue">Size worn:<br/>Rented For:</p>
+                          <div className="col-xs-12 col-sm-8 col-md-9 col-lg-9">
+                              <Link to={`/profile/${data.userId}`}>
+                                  <h4 className="comment_username">{data.name} <span style={{fontSize:'15px'}}>(Renter)</span></h4><br/>
+                              </Link>
                           </div>
-                          <div className="col-md-3 d_size">
-                            <p><span className="dec">{data.size}</span><br/><span className="dec">{data.wear}</span></p>
+                          <div className="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+                            <p className="">{data.date}</p>
                           </div>
-                      </div><br/>
-                      <p className="tissue">more <br/> detail</p>
-
-                    </div>{/*Col md 3 close*/}
-                    <div className="col-md-6 tissue2">
+                      </div>    
+                      <div className="row" style={{margin:'0px'}}>
+                          <div className="col-xs-12 col-sm-4 col-md-3 col-lg-3" style={{backgroundColor:'#e1e1e1'}}>
+                            <p className="comment_dress_size">Size worn: {data.size}</p>
+                          </div>
+                          <div className="col-xs-12 col-sm-3 col-md-4 col-lg-5"></div>
+                          <div className="col-xs-12 col-sm-5 col-md-5 col-lg-4 marg_mobile">
+                              <Rating className="col-md-8" 
+                                emptySymbol="glyphicon glyphicon-star-empty"
+                                fullSymbol="glyphicon glyphicon-star"
+                                initialRating={data.rate} 
+                                readonly
+                                style={{color: '#cb9d6c'}}
+                              />                          
+                              <p className="card">4.5</p>
+                              <span style={{marginLeft:"10px"}}><sub className="dar"></sub></span>
+                          </div>
+                      </div>   
                       <div className="row">
-                        <div className="col-md-7 sung" >
-                          <Rating className="col-md-8" 
-                              emptySymbol="glyphicon glyphicon-star-empty"
-                              fullSymbol="glyphicon glyphicon-star"
-                              initialRating={data.rate} 
-                              readonly
-                              style={{color: 'white'}}
-                          />                          
-                          <span className="card">Ratings</span>
-                          <span style={{marginLeft:"10px"}}><sub className="dar"></sub></span>
-
-                        </div>
-                        <div className="col-md-5 sung"><span>{data.date}</span></div>
-                      </div><br/><br/>{/*Inner Row Closed*/}
-
-                      <div className="row">
-                        {/*<div className="col-md-6 sungg">
-                          <h4>Stunning I enjoyed</h4>
-                        </div>*/}
-                      </div>{/*inner row*/}
-                       <br/><br/>
-                       <p className="sung">{data.msg}</p>
-                    </div>
-                    <div className="col-md-3 shift">
-                        <img src={data.img} style={{width:"90%" ,padding: '10%'}}/>
-                        
-                    </div>{/*Col md 3 close*/}                
-                  </div>{/*col-md-12 closed*/}
-                </div>{/*Main Row Div container Closed*/}
-              </div>{/*Main Row closed*/}
-              <br/>
-              <div className="row action visible-sm visible-xs">
-                <div className="container">
-                  <div className="row">
-                    <h4 className="sungg" style={{fontFamily: 'Sacramento',fontSize: '30px'}}>{data.name}</h4><br/>
-                     <div className="row">
-                        <div className="col-sm-3 col-xs-7">
-                          <p className="tissue">Size worn:<br/>Rented For:</p>
-                        </div>
-                        <div className="col-sm-2 col-xs-5 d_size">
-                          <p><span className="dec">{data.size}</span><br/><span className="dec">{data.wear}</span></p>
-                        </div>
-                        <div className="col-sm-12 col-xs-12">
-                          <p className="tissue">more <br/> detail</p>
-                        </div>
-                        
-                     </div>
-
-                     
-
-                    <div className="row">
-                      <div className="col-sm-6 col-xs-12">
-                        <div className="derr">
-                          <Rating className="col-md-7 col-xs-8" 
-                              emptySymbol="glyphicon glyphicon-star-empty"
-                              fullSymbol="glyphicon glyphicon-star"
-                              initialRating={data.rate} 
-                              readonly
-                              style={{color: 'white'}}
-                          />
-                          <span className="card">Ratings</span>
-                          <span style={{marginLeft:"10px"}}><sub className="dar"></sub></span>
-                        </div><br/>     
-                        <div className="sungg">
-                        </div> 
+                          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                              <p className="comment_dress_details">{data.msg}</p>
+                          </div>
                       </div>
-                      <div className="col-sm-6 col-xs-12">
-                        <div className="col-md-6 sung"><span>{data.date}</span></div>
-                      </div><br/><br/><br/>
-                      <div className="row"><br/><br/>
-                        <div className= "container-fluid point">
-                          <p className="point">{data.msg}</p>
-                        </div>      
-                      </div><br/>{/*row closed*/}
-                      <div className="row derr">
-                        <img src={data.img}/> 
-                      </div>  
-                    </div>{/*Row Closed*/}
-                  </div>{/*row closed*/}
-                </div>{/*main container row closed*/}
-              </div>{/*Main row Closed*/}
+                          <br/>
+                          {/*<p className="">{data.wear}</p>*/}
+                  </div>
+                  <div className="col-xs-12 col-sm-2 col-md-3 col-lg-3">
+                      <img src={data.img}/>
+                  </div>             
               </div>
+              <br/>
+          </div>
           );
 
     }
